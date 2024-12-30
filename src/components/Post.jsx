ee5db507b36342d19e7bff110eeebd79a25734ba
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-12-30 21:49:58 +1100

    done

---
+++ src/components/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.body}>{props.body}</p>
+    </div>
+  );
+};
+
+export default Post;
